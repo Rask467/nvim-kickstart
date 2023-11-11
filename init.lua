b382138dd26a18d15a69e3f49a3c072a1e338264
Rask467 <95650694+Rask467@users.noreply.github.com>
2023-11-11 08:25:29 -0700

    Add rust_analyzer

--- init.lua
+++ init.lua
@@ -489,7 +489,7 @@ local servers = {
   -- clangd = {},
   gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
